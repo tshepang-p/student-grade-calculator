6cf9a1db5c58f83fb34b1461b3f10d5bfcec0824
Tshepang Phakani <tshepangPhakani01@gmail.com>
2026-05-17 23:14:20 +0200

    error 1 @ line 106, GradeCalculator.java. Changed the less than sign to more than

--- src/main/java/com/vut/calculator/GradeCalculator.java
+++ src/main/java/com/vut/calculator/GradeCalculator.java
@@ -103,7 +103,7 @@ public class GradeCalculator {
         }
         double highest = marks[0];
         for (int i = 1; i < marks.length; i++) {
-            if (marks[i] < highest) {
+            if (marks[i] > highest) {
                 highest = marks[i];
             }
         }
